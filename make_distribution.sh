849744fe0c4e1bdd26787c7412e718f8e5c9692e
Ian Horswill <ian@northwestern.edu>
2014-11-21 17:49:20 -0600

    Fixes to make_distribution script

--- make_distribution.sh
+++ make_distribution.sh
@@ -4,4 +4,5 @@ cp -r Documentation /tmp/Craft_distribution
 cp Craft/bin/Release/Craft.dll /tmp/Craft_distribution
 cp Unity/*.cs Unity/*.unity /tmp/Craft_distribution
 cp *.md /tmp/Craft_distribution
-zip -r /tmp/Craft_distribution.zip /tmp/Craft_distribution
+cd /tmp
+zip -r Craft_$1.zip Craft_distribution
